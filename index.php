516c7c24049204a5678e3f95ff9429a1f21e2fbc
BastienB <bastbenech@gmail.com>
2020-12-06 03:28:16 +0100

    Initialisation du depot

---
+++ index.php
@@ -0,0 +1,34 @@
+<!DOCTYPE HTML>
+<html lang="fr">
+    <HEAD>
+        <link rel="stylesheet" type="text/css" href="mystyle.css" />
+        <meta charset="utf-8">
+        <title>Projet PHP</title>
+    </HEAD>
+    <BODY>
+
+        <div class="divTitre">
+            <img id="imgBandeau" src="libImages/logo iut 2018.jpg">
+            <h1>Boutique de CDs</h1>
+            <p>par Bastien Benech</p>
+        </div>
+
+        <div class="divListeCD">
+			<ul>
+				<div class="divOffreCD">
+					<div class="divImgOffreCD">
+						<img class="imgOffreCD" src="libImages/JVLIVS.jpg">
+					</div>
+					<div class="divInfoOffreCD">
+						<p class="titreOffreCD">JVLIVS</p>
+						<p class="auteurOffreCD">SCH</p>
+						<p class="genreOffreCD">Rap / Hip-Hop français</p>
+						<p class="prixOffreCD">18,00 €</p>
+					</div>
+				</div>
+			</ul>
+		</div>
+        
+	</BODY>
+</html>
+</HTML>
