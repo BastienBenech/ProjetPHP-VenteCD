6e6cc30ff16fdceb0f52d9877d1cfdaa674f386d
BastienB <bastbenech@gmail.com>
2020-12-06 15:29:57 +0100

    acceuil affichant les donnees xml finit

--- index.php
+++ index.php
@@ -13,24 +13,32 @@
             <p>par Bastien Benech</p>
         </div>
 
-        <div class="divListeCD">
-			<div class="divOffreCD">
-				<div class="divImgOffreCD">
-					<img class="imgOffreCD" src="libImages/JVLIVS.jpg">
-				</div>
-				<div class="divInfoOffreCD">
-					<p class="titreOffreCD"><strong>Titre :</strong> JVLIVS</p>
-					<p class="auteurOffreCD"><strong>Auteur :</strong> SCH</p>
-					<p class="genreOffreCD"><strong>Genre :</strong> RAP / Hip-Hop français</p>
-					<div class="divPrixOffreCD">
-						<p class="prixOffreCD">18,00 €</p>
-						<form action="ficheCD.php" method="GET">
-							<input type="submit" value="Détail" class="btnFicheCD">
-						</form>
-					</div>
-				</div>
-			</div>
-		</div>
+		<?php
+			$lesAlbums = simplexml_load_file('donneesCD.xml');
+
+			foreach($lesAlbums->album as $cd)
+			{
+				echo
+				"<div class='divListeCD'>",
+					"<div class='divOffreCD'>",
+						"<div class='divImgOffreCD'>",
+							"<img class='imgOffreCD' src='libImages/$cd->pochette'>",
+						"</div>",
+						"<div class='divInfoOffreCD'>",
+							"<p class='titreOffreCD'><strong>Titre :</strong> $cd->titre</p>",
+							"<p class='auteurOffreCD'><strong>Auteur :</strong> $cd->auteur</p>",
+							"<p class='genreOffreCD'><strong>Genre :</strong> $cd->genre</p>",
+							"<div class='divPrixOffreCD'>",
+								"<p class='prixOffreCD'>$cd->prix €</p>",
+								"<form action='ficheCD.php' method='GET'>",
+									"<input type='submit' value='Détail' class='btnFicheCD'>",
+								"</form>",
+							"</div>",
+						"</div>",
+					"</div>",
+				"</div>";
+			}
+		?>
 	</BODY>
 </html>
 </HTML>
