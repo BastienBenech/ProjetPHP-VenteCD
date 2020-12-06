928ee6cd87b23a5cc6f971feac2304481fa85d3c
BastienB <bastbenech@gmail.com>
2020-12-06 10:58:50 +0100

    validation pour push

--- index.php
+++ index.php
@@ -14,21 +14,23 @@
         </div>
 
         <div class="divListeCD">
-			<ul>
-				<div class="divOffreCD">
-					<div class="divImgOffreCD">
-						<img class="imgOffreCD" src="libImages/JVLIVS.jpg">
-					</div>
-					<div class="divInfoOffreCD">
-						<p class="titreOffreCD">JVLIVS</p>
-						<p class="auteurOffreCD">SCH</p>
-						<p class="genreOffreCD">Rap / Hip-Hop français</p>
+			<div class="divOffreCD">
+				<div class="divImgOffreCD">
+					<img class="imgOffreCD" src="libImages/JVLIVS.jpg">
+				</div>
+				<div class="divInfoOffreCD">
+					<p class="titreOffreCD"><strong>Titre :</strong> JVLIVS</p>
+					<p class="auteurOffreCD"><strong>Auteur :</strong> SCH</p>
+					<p class="genreOffreCD"><strong>Genre :</strong> RAP / Hip-Hop français</p>
+					<div class="divPrixOffreCD">
 						<p class="prixOffreCD">18,00 €</p>
+						<form action="ficheCD.php" method="GET">
+							<input type="submit" value="Détail" class="btnFicheCD">
+						</form>
 					</div>
 				</div>
-			</ul>
+			</div>
 		</div>
-        
 	</BODY>
 </html>
 </HTML>
